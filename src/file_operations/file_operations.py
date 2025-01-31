9977d1395aa9aadc646dc68d656817a69d321952
philipp-hinteregger <philipp.hinteregger@dynatrace.com>
2025-01-31 08:58:30 +0100

    feat: add file operations module

---
+++ src/file_operations/file_operations.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+import yaml
+
+def copy_and_replace_yaml(src, dst, replacements):
+    shutil.copy(src, dst)
+    with open(dst, 'r') as file:
+        data = yaml.safe_load(file)
+    for key, value in replacements.items():
+        if key in data:
+            data[key] = value
+    with open(dst, 'w') as file:
+        yaml.safe_dump(data, file)
+
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    else:
+        print(f"The file {file_path} does not exist.")
